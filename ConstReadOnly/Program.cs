c5ccf47855eba0c0fe11da5ca97f929a0303479e
nicolasandreti <nicolasandreti@gmail.com>
2023-12-20 16:52:38 +0700

    Update Day3

---
+++ ConstReadOnly/Program.cs
@@ -0,0 +1,29 @@
+﻿class Car
+{
+	public readonly int year;
+	public const string brand = "Toyota"; //Const wajib diisi terlebih dahulu valuenya
+	
+	public Car(int year)
+	{
+		this.year = year;
+	}
+	public void GetYear()
+	{
+		Console.WriteLine(year);
+	}
+	public void GetBrand()
+	{
+		Console.WriteLine(brand);
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Car car = new Car(2009);
+		car = new Car(2008);
+		car.GetYear();
+		car.GetBrand();
+	}
+}
