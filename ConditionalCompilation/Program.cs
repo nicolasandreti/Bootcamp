a0d2cb011fc3a4b5ec8c872bdc998223a1faf48a
nicolasandreti <nicolasandreti@gmail.com>
2024-01-18 09:42:07 +0700

    Update Day17

---
+++ ConditionalCompilation/Program.cs
@@ -0,0 +1,20 @@
+﻿#define BCD
+#define CDD
+
+class Program
+{
+	static void Main()
+	{
+		
+		#if ABC
+		Console.WriteLine("Hello, ABC!");
+		
+		#elif BCD
+		Console.WriteLine("Hello, BCD!");
+		
+		#else
+		Console.WriteLine("Hello, CDE!");
+		
+		#endif
+	}
+}
---
+++ StringIteration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+	static void Main()
+	{
+		string a  = String.Empty;
+		int iteration = 10000;
+		
+		Stopwatch stopwatch = new();
+		stopwatch.Start();
+		for(int i = 0; i<iteration; i++)
+		{
+			a += "Hello";
+			a += "World";
+		}
+		stopwatch.Stop();
+		Console.WriteLine(stopwatch.ElapsedMilliseconds);
+	}
+}
