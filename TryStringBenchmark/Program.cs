137309a4cddd36f34e70430dc8154d834e34612c
nicolasandreti <nicolasandreti@gmail.com>
2024-01-18 14:54:26 +0700

    UpdateFromMaterialDay18

--- TryStringBenchmark/Program.cs
+++ TryStringBenchmark/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+
+class Program
+{
+	static void Main() 
+    {
+        BenchmarkRunner.Run<StringBenchmarker>();
+    }
+}
